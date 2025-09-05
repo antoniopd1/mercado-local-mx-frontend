28b14b46858ae2134ca1dc9942386cdc393bf651
Antonio Perez <antoniocan491@gmail.com>
2025-09-05 17:42:30 -0600

    bg imagen fixed

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -64,7 +64,7 @@ function Dashboard() {
     }
 
     return (
-        <div className="min-h-screen flex flex-col bg-gray-100 bg-cover bg-center background-attachment: fixed"
+        <div className="bg-fixed min-h-screen flex flex-col bg-gray-100 bg-cover bg-center"
             style={{ backgroundImage: 'url("/Uriangato-Guanajuato-principal-1200x500.jpg")' }}
         >
             {/* Navbar */}
